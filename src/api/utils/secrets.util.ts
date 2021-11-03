0bcc5d3b973256d3a38dbfe5d05bd138e985e566
Jamie Samuel <jsam07@hotmail.com>
2021-11-03 01:13:18 -0700

    Add environment variables to workflows

--- src/api/utils/secrets.util.ts
+++ src/api/utils/secrets.util.ts
@@ -2,17 +2,16 @@ import dotenv from 'dotenv';
 import IOHandler from './iohandler.util';
 import { logger } from './logger.util';
 
-if (IOHandler.dirExists('.env')) {
-    logger.info('Using .env file to supply config environment variables');
-    dotenv.config({ path: '.env' });
-} else {
-    logger.info(
-        'Using .env.example file to supply config environment variables',
-    );
-    dotenv.config({ path: '.env.example' });
-}
-
 export const PORT = process.env.PORT || 3000;
 export const { WIKI_USERNAME } = process.env || '';
 export const { WIKI_PASSWORD } = process.env || '';
 export const ENVIRONMENT = process.env.NODE_ENV || 'development';
+
+if (IOHandler.dirExists('.env')) {
+    logger.info('Using .env file to supply config environment variables');
+    dotenv.config({ path: '.env' });
+} else if (WIKI_PASSWORD && WIKI_PASSWORD) {
+    logger.info('Environment variables loaded successfully');
+} else {
+    logger.warn('Unable to load all required environment variables');
+}
